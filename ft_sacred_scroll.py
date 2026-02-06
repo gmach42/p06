00f1f6330b31b1d4c37edc9abbdb8d9ac949fb04
gmach <gmach@student.42lyon.fr>
2026-02-06 09:53:02 +0100

    - instead of :

--- ft_sacred_scroll.py
+++ ft_sacred_scroll.py
@@ -19,29 +19,29 @@ def test_sacred_scroll() -> None:
         print("alchemy.elements.create_air(): ", end="")
         print(alchemy.elements.create_air())
     except AttributeError:
-        print("AttributeError: not exposed")
+        print("AttributeError - not exposed")
 
     print("\nTesting package-level access: (controlled by __init__.py):")
     try:
         print("alchemy.create_fire(): ", end="")
         print(alchemy.create_fire())
     except AttributeError:
-        print("AttributeError: not exposed")
+        print("AttributeError - not exposed")
     try:
         print("alchemy.create_water(): ", end="")
         print(alchemy.create_water())
     except AttributeError:
-        print("AttributeError: not exposed")
+        print("AttributeError - not exposed")
     try:
         print("alchemy.create_earth(): ", end="")
         print(alchemy.create_earth())
     except AttributeError:
-        print("AttributeError: not exposed")
+        print("AttributeError - not exposed")
     try:
         print("alchemy.create_air(): ", end="")
         print(alchemy.create_air())
     except AttributeError:
-        print("AttributeError: not exposed")
+        print("AttributeError - not exposed")
 
     print("\nPackage metadata:")
     print(f"Version: {alchemy.__version__}")
